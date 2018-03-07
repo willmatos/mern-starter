f64f06719a617c5aa089000bc25d180cda4a7d3b
Will Matos <will.matos@gmail.com>
2018-03-07 06:57:21 -0600

    Add dummy data for Metrics and Dimensions to the database seeder file.

--- server/dummyData.js
+++ server/dummyData.js
@@ -1,6 +1,103 @@
+import cuid from 'cuid';
+
+import countryCodesIso2 from './countryCodesData';
+
 import Post from './models/post';
+import Metric from './models/metric';
+import Dimension from './models/dimension';
 
 export default function () {
+  Metric.count().exec((err, count) => {
+    if (count > 0) {
+      return;
+    }
+
+    // lets make x records
+    const numberOfMetricRecordsToMake = 100;
+    const metricsToCreate = [];
+
+    for (let i = 0; i < numberOfMetricRecordsToMake; i++) {
+      const newMetric = new Metric();
+
+      newMetric.source_id = cuid();
+
+      newMetric.name = `Metric Name ${i}`;
+      newMetric.status = Math.floor(Math.random() * Math.floor(3));
+
+      metricsToCreate.push(newMetric);
+    }
+
+    Metric.collection.insert(metricsToCreate, (metricsInsertError, metrics) => {
+      if (metricsInsertError) {
+        // TODO: handle error
+      } else {
+        // let's add dimensions to these metrics
+        metrics.ops.forEach((metric) => {
+          // let's add some dimensions for this metric
+          const numberOfDimensionRecordsToMake = Math.floor(Math.random() * Math.floor(25));
+          const dimensionsToCreate = [];
+
+          for (let j = 0; j < numberOfDimensionRecordsToMake; j++) {
+            const newDimension = new Dimension();
+
+            newDimension.name = countryCodesIso2[Math.floor(Math.random() * Math.floor(countryCodesIso2.length))];
+            newDimension.latest_value = Math.floor(Math.random() * Math.floor(100));
+            newDimension.metric = metric;
+
+            dimensionsToCreate.push(newDimension);
+          }
+
+          Dimension.collection.insert(dimensionsToCreate, (dimensionsInsertError, dimensions) => {
+            if (dimensionsInsertError) {
+              // TODO: handle error
+            } else {
+              // let's add dimensions to metric's dimensions collection
+              const dimensionObjectIds = [];
+
+              dimensions.ops.forEach((dimension) => {
+                dimensionObjectIds.push(dimension._id);
+              });
+
+              Metric.findOne({ _id: metric._id }, (metricFindOneError, doc) => {
+                const metricToSave = doc;
+                metricToSave.dimensions = dimensionObjectIds;
+                metricToSave.save();
+              });
+            }
+          });
+        });
+      }
+    });
+
+
+    // let's add the dependent_metric_ids to the metrics
+    Metric.find({}, (metricFindAllError, metrics) => {
+      if (metricFindAllError) {
+        // TODO: handle error
+      } else {
+        metrics.forEach((metricToUpdate) => {
+          const numberOfMetricRecordsToRelate = Math.floor(Math.random() * Math.floor(25));
+          const metricsToRelateObjectIds = [];
+
+          Metric.find({}, (relatedMetricsFindError, relatedMetrics) => {
+            relatedMetrics.forEach((relatedMetric) => {
+              if (relatedMetric._id !== metricToUpdate._id) {
+                metricsToRelateObjectIds.push(relatedMetric._id);
+              }
+            });
+          }).limit(numberOfMetricRecordsToRelate);
+
+          Metric.findOne({ _id: metricToUpdate._id }, (metricFindOneError, doc) => {
+            const metricToSave = doc;
+            metricToSave.dependent_metrics_ids = metricsToRelateObjectIds;
+            metricToSave.save();
+          });
+        });
+      }
+    });
+  });
+
+  // Boilerplate Code
   Post.count().exec((err, count) => {
     if (count > 0) {
       return;
